a74b2842bc6153048716746aff27206eb1a8f11e
Vedant Gawas <vedantgawas028@gmail.com>
2026-01-14 18:29:06 +0530

    Two sum solution with hashmap approch aded

--- hashmap/1_TwoSum.java
+++ hashmap/1_TwoSum.java
@@ -6,13 +6,15 @@
 class Solution { 
     public int[] twoSum(int[] nums, int target){
         HashMap<Integer,Integer> map = new HashMap<>();
-            for(int i = 0; i < nums.length; i++){
+            for(int i = 0; i < nums.length; i++){ //logic to check if complement exists in map
             int complement = target - nums[i];
             if(map.containsKey(complement)){
-                return  new int[] {map.get(complement),i};
+                return  new int[] {map.get(complement),i}; //return indices of complement and current element
             }
-            map.put(nums[i],i);
+            map.put(nums[i],i); //store element and its index in map
     }
     return new int[] {};
     }
-}
+}//time complexity O(n)
+//space complexity O(n)
+//hashmap used to store the elements and their indices while iterating through the array only once.
