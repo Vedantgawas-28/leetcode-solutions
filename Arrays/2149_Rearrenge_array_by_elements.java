dd09883a3d210a483511280f1d0da52e921ec4c2
Vedant Gawas <vedantgawas028@gmail.com>
2026-01-14 18:26:33 +0530

    push origin main

--- Arrays/2149_Rearrenge_array_by_elements.java
+++ Arrays/2149_Rearrenge_array_by_elements.java
@@ -1,11 +1,11 @@
 class Solution {
     public int[] rearrangeArray(int[] nums) {
-        int n=nums.length;
-        int[] answer= new int[n];
-        int pos_index= 0;
+        int n=nums.length;         
+        int[] answer= new int[n]; 
+        int pos_index= 0; 
         int neg_index=1;
         
-        for(int i=0;i<n;i++){
+        for(int i=0;i<n;i++){ //if no is positive place it at even index else at odd index 
             if(nums[i]>0){
                 answer[pos_index]=nums[i];
                 pos_index+=2;
@@ -17,4 +17,5 @@ class Solution {
         return answer;
         
     }
-}
+}//time complexity O(n)
+//space complexity O(n)
