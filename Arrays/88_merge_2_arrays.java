2ac1faa7adb58e570adfa6e0ade8276b2fc7b370
Vedant Gawas <vedantgawas028@gmail.com>
2026-01-16 22:28:31 +0530

    some comments added for understanding

--- Arrays/88_merge_2_arrays.java
+++ Arrays/88_merge_2_arrays.java
@@ -1,25 +1,26 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
 
-        int i = m - 1;
-        int j = n - 1;
-        int k = m + n - 1;
+        int i = m - 1;   //` Pointer for the end of nums1's initial elements
+        int k = m + n - 1;// Pointer for the end of nums1
+        int j = n - 1; // Pointer for the end of nums2
 
-        while (i >= 0 && j >= 0) {
-            if (nums1[i] > nums2[j]) {
-                nums1[k] = nums1[i];
+        while (i >= 0 && j >= 0) {  // Merge in reverse order
+            if (nums1[i] > nums2[j]) {  // Compare elements from the end
+                nums1[k] = nums1[i]; // Place larger element at the end of nums1
                 i--;
             } else {
-                nums1[k] = nums2[j];
-                j--;
+                nums1[k] = nums2[j]; // Place larger element at the end of nums1
+                j--;   
             }
             k--;
         }
 
         while (j >= 0) {
-            nums1[k] = nums2[j];
+            nums1[k] = nums2[j];   // Copy remaining elements from nums2, if any
             j--;
             k--;
         }
     }
-}
+}//Time complexity: O(m+n)
+//Space complexity: O(1)
--- Linked list/876_middleList.java
+++ Linked list/876_middleList.java
@@ -1,22 +1,14 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
+
 class Solution {
     public ListNode middleNode(ListNode head) {
         ListNode slow=head;
         ListNode fast=head;
 
         while(fast!=null && fast.next!=null){
-            slow=slow.next;
-            fast=fast.next.next;
+            slow=slow.next;//move slow by 1
+            fast=fast.next.next;//move fast by 2
         }
         return slow;
     }
-}
+}//Time complexity: O(n)
+//Space complexity: O(1)
