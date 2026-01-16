8b83f07b8d7568dc6436a237fdf4840cd258e76e
Vedant Gawas <vedantgawas028@gmail.com>
2026-01-16 21:56:23 +0530

    some comments added for understanding

--- Linked list/206_RiverseLinked_list.java
+++ Linked list/206_RiverseLinked_list.java
@@ -11,7 +11,7 @@
 class Solution {
     public ListNode reverseList(ListNode head) {
         ListNode prev= null;
-        ListNode curr= head;gi
+        ListNode curr= head;
     while(curr !=null){
         ListNode next= curr.next;//store next node
         curr.next=prev;//reverse the link list 
