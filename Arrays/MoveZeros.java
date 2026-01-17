05a28fd79139ccd707abe40bfad20175a203251a
Vedant Gawas <vedantgawas028@gmail.com>
2026-01-17 20:44:07 +0530

    saved changes

--- Arrays/MoveZeros.java
+++ Arrays/MoveZeros.java
@@ -4,9 +4,10 @@ class Solution {
         for(int i=0;i<nums.length;i++){
             if(nums[i]!=0){
                 int temp=nums[i];
-                nums[i]=nums[j];
-                nums[j]=temp;
+                nums[i]=nums[j];//swap
+                nums[j]=temp;//
                 j++;
             }
         }
-    }
+    }//Time complexity: O(n)
+    //Space complexity: O(1)
