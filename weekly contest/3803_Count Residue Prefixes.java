3445b3b59a0ebec0f6babb10b76c34e1850f43b9
Vedant Gawas <vedantgawas028@gmail.com>
2026-01-17 20:49:37 +0530

    residue added

---
+++ weekly contest/3803_Count Residue Prefixes.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int residuePrefixes(String s) {
+        Set<Character> seen= new HashSet<>();
+        int residue=0;
+
+        for(int i=0;i<s.length();i++){
+            seen.add(s.charAt(i));
+            int distinct= seen.size();
+            int mod= (i+1)%3;
+
+            if(mod==distinct){
+                residue++;
+            }
+        }
+        return residue;
+    }
+}
