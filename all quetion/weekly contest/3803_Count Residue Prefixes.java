1f4bee461acb468badc4d62d422c7a2751a2b88a
Vedant Gawas <vedantgawas028@gmail.com>
2026-01-20 19:13:50 +0530

    new folder

--- all quetion/weekly contest/3803_Count Residue Prefixes.java
+++ all quetion/weekly contest/3803_Count Residue Prefixes.java
@@ -1,9 +1,10 @@
 class Solution {
     public int residuePrefixes(String s) {
         Set<Character> seen= new HashSet<>();
-        int residue=0;
-
-        for(int i=0;i<s.length();i++){
+        int residue=0;      /*number of distinct characters
+                            ==
+                            (length of the prefix % 3)*/
+            for(int i=0;i<s.length();i++){
             seen.add(s.charAt(i));
             int distinct= seen.size();
             int mod= (i+1)%3;
